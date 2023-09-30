bb053a7130a59ffd24b9fc651528e2b99b38abca
abhisheksr01 <abhisheksingh2691@gmail.com>
2023-09-30 20:59:32 +0100

    refactor: extract commands from Makefile to bash scripts

--- db/upload.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-psql -h localhost -U postgres < ./opt/rates.sql
-psql -h localhost -U postgres -c "SELECT 'alive'"
-psql -h localhost -U postgres -c "SELECT current_database();"
