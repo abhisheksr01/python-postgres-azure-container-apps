527675abc96f522ffe9485b7a4896ac7dfb258b5
abhisheksr01 <abhisheksingh2691@gmail.com>
2023-10-02 22:54:25 +0100

    ci: export OIDC variables as env in infra pipeline

--- infrastructure/bootstrap/main.tf
+++ infrastructure/bootstrap/main.tf
@@ -11,12 +11,5 @@ terraform {
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
-  use_oidc        = true
-  subscription_id = sensitive(var.subscription_id)
-  client_id       = sensitive(var.client_id)
-  tenant_id       = sensitive(var.tenant_id)
-  # for GitHub Actions
-  oidc_request_token = var.oidc_request_token
-  oidc_request_url   = var.oidc_request_url
   features {}
 }
