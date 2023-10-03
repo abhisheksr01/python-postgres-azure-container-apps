518e4fe4af5ae489b95cff8b9a5d8f05515a6f3c
abhisheksr01 <abhisheksingh2691@gmail.com>
2023-10-03 13:50:01 +0100

    fix: TF fmt error

--- infrastructure/bootstrap/service-principal.tf
+++ infrastructure/bootstrap/service-principal.tf
@@ -2,7 +2,7 @@ resource "azuread_application" "python_postgres_azure_app" {
   display_name = "${var.resource_name_prefix}-github-oidc"
   owners       = [data.azuread_client_config.current.object_id]
   tags         = var.default_set_tags
-    lifecycle {
+  lifecycle {
     ignore_changes = [owners]
   }
 }
